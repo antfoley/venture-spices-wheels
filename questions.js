d5c6989a8477dfd9bc087a36ab53d8a22246d13f
Anthony Foley <antfoley@gmail.com>
2025-09-18 17:45:14 +0100

    initial commit

---
+++ questions.js
@@ -0,0 +1,50 @@
+const QUESTIONS = {
+    Social: [
+      "Do you have the right skills and attitudes to build friendships?",
+      "Can you communicate with others?",
+      "Do you try to understand other cultures that you come into contact with?",
+      "Do you get involved in your community?",
+      "Can you understand how the Promise and Law fits into everyday life?"
+    ],
+    Physical: [
+      "Do you have a balanced diet?",
+      "Do you know the importance of having good personal hygiene and how it affects others?",
+      "Do you make time for physical activity?",
+      "How good are you at getting medical help when you are not feeling well?",
+      "Do you appreciate that physical differences make society better?",
+      "Do you understand the impact your choices have on your life?",
+      "Do you know the affects of abuse on the human body?"
+    ],
+    Intellectual: [
+      "Are you able to solve problems?",
+      "Do you see how you can learn something from every new situation?",
+      "Can you think outside of the box and try your own creative ideas?",
+      "Can you plan, carry out and review a project?",
+      "Do you know what qualities you bring to a team?"
+    ],
+    Character: [
+      "Do you put the Promise and Law into practice?",
+      "Do you know what’s important in your life?",
+      "Do you try your best in everything that you do?",
+      "Can you stand up for what you believe in?",
+      "Do you treat others with respect?",
+      "Do you try to help where you see injustice or inequality?",
+      "Do you have the courage to reach for your goals?",
+      "Do you embrace the Scouting Spirit?"
+    ],
+    Emotional: [
+      "Can you deal with having an off day?",
+      "Do you call on others when you have a problem?",
+      "Do you listen when someone has a problem?",
+      "Can you show your feelings in a positive way?",
+      "Do you have the confidence to deal with all that life can throw at you?",
+      "Do you stand up for what you believe in?",
+      "Do you try to develop your talents?"
+    ],
+    Spiritual: [
+      "Do you stand by your spiritual beliefs?",
+      "Do you make a positive impact on the environment?",
+      "Do you see God in the world around you?",
+      "Do you see spirituality as a vital part of your life?"
+    ]
+  };
